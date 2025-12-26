5cc9bbf10dab131c8afea7e5760c5f6ab2bda8c8
Lee Inho <inhoking01@gmail.com>
2025-12-26 23:02:52 +0900

    git init

---
+++ OMT/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  OMT
+//
+//  Created by 이인호 on 12/26/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ OMT/OMTApp.swift
@@ -0,0 +1,17 @@
+//
+//  OMTApp.swift
+//  OMT
+//
+//  Created by 이인호 on 12/26/25.
+//
+
+import SwiftUI
+
+@main
+struct OMTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
