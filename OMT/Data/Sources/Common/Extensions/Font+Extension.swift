b0e2582b229706416118c4b416004007bd0a476e
Lee Inho <inhoking01@gmail.com>
2026-01-21 01:05:50 +0900

    chore. extension에 커스텀 폰트 등록

---
+++ OMT/Data/Sources/Common/Extensions/Font+Extension.swift
@@ -0,0 +1,85 @@
+//
+//  Font+Extension.swift
+//  OMT
+//
+//  Created by 이인호 on 1/21/26.
+//
+
+import SwiftUI
+
+extension Font {
+    // Pretendard
+    static func pretendardBlack(size: CGFloat) -> Font {
+        .custom("Pretendard-Black", size: size)
+    }
+    
+    static func pretendardBold(size: CGFloat) -> Font {
+        .custom("Pretendard-Bold", size: size)
+    }
+    
+    static func pretendardExtraBold(size: CGFloat) -> Font {
+        .custom("Pretendard-ExtraBold", size: size)
+    }
+    
+    static func pretendardExtraLight(size: CGFloat) -> Font {
+        .custom("Pretendard-ExtraLight", size: size)
+    }
+    
+    static func pretendardLight(size: CGFloat) -> Font {
+        .custom("Pretendard-Ligth", size: size)
+    }
+    
+    static func pretendardMedium(size: CGFloat) -> Font {
+        .custom("Pretendard-Medium", size: size)
+    }
+    
+    static func pretendardRegular(size: CGFloat) -> Font {
+        .custom("Pretendard-Regular", size: size)
+    }
+    
+    static func pretendardSemiBold(size: CGFloat) -> Font {
+        .custom("Pretendard-SemiBold", size: size)
+    }
+    
+    static func pretendardThin(size: CGFloat) -> Font {
+        .custom("Pretendard-Thin", size: size)
+    }
+    
+    // Paperlogy
+    static func paperlogyThin(size: CGFloat) -> Font {
+        .custom("Paperlogy-1Thin", size: size)
+    }
+    
+    static func paperlogyExtraLight(size: CGFloat) -> Font {
+        .custom("Paperlogy-2ExtraLight", size: size)
+    }
+    
+    static func paperlogyLight(size: CGFloat) -> Font {
+        .custom("Paperlogy-3Light", size: size)
+    }
+    
+    static func paperlogyRegular(size: CGFloat) -> Font {
+        .custom("Paperlogy-4Regular", size: size)
+    }
+    
+    static func paperlogyMedium(size: CGFloat) -> Font {
+        .custom("Paperlogy-5Medium", size: size)
+    }
+    
+    static func paperlogySemiBold(size: CGFloat) -> Font {
+        .custom("Paperlogy-6SemiBold", size: size)
+    }
+    
+    static func paperlogyBold(size: CGFloat) -> Font {
+        .custom("Paperlogy-7Bold", size: size)
+    }
+    
+    static func paperlogyExtraBold(size: CGFloat) -> Font {
+        .custom("Paperlogy-8ExtraBold", size: size)
+    }
+    
+    static func paperlogyBlack(size: CGFloat) -> Font {
+        .custom("Paperlogy-9Black", size: size)
+    }
+}
+
