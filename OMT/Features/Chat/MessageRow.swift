bc13a1037a1f94c9e9ca530365604fffe92bfd8b
Lee Inho <inhoking01@gmail.com>
2026-01-28 00:33:13 +0900

    chore. messagerow 디자인 적용

--- OMT/Features/Chat/MessageRow.swift
+++ OMT/Features/Chat/MessageRow.swift
@@ -15,20 +15,36 @@ struct MessageRow: View {
             if message.isFromUser {
                 Spacer()
                 Text(message.content)
-                    .padding(.horizontal, 16)
-                    .padding(.vertical, 10)
-                    .background(Color.blue)
-                    .foregroundColor(.white)
-                    .cornerRadius(18)
+                    .typography(.sub_b2_4)
+                    .padding(.horizontal, 12)
+                    .padding(.vertical, 11)
+                    .foregroundColor(.gray12)
+                    .background(.primary5)
+                    .clipShape(
+                        UnevenRoundedRectangle(
+                            topLeadingRadius: 12,
+                            bottomLeadingRadius: 12,
+                            bottomTrailingRadius: 0,
+                            topTrailingRadius: 12
+                        )
+                    )
                     .frame(maxWidth: 250, alignment: .trailing)
             } else {
                 if message.type == .text {
                     Text(message.content)
+                        .typography(.sub_b2_4)
                         .padding(.horizontal, 16)
-                        .padding(.vertical, 10)
-                        .background(Color.gray.opacity(0.2))
-                        .foregroundColor(.primary)
-                        .cornerRadius(18)
+                        .padding(.vertical, 11)
+                        .background(.gray2)
+                        .foregroundColor(.gray12)
+                        .clipShape(
+                            UnevenRoundedRectangle(
+                                topLeadingRadius: 0,
+                                bottomLeadingRadius: 12,
+                                bottomTrailingRadius: 12,
+                                topTrailingRadius: 12
+                            )
+                        )
                         .frame(maxWidth: 250, alignment: .leading)
                 } else {
                     VStack(alignment: .leading) {
@@ -41,23 +57,30 @@ struct MessageRow: View {
                             Text("채팅봇") // 봇 이름
                         }
                         
-                        VStack(alignment: .leading) {
+                        VStack(alignment: .leading, spacing: 20) {
                             Text(message.content)
-                                .padding(.bottom, 10)
+                                .typography(.sub_b2_4)
+                                .foregroundColor(.gray12)
                             
-                            VStack {
+                            VStack(spacing: 10) {
                                 if let options = message.options {
                                     ForEach(options, id: \.self) { option in
                                         Button {
                                             print(option)
                                         } label: {
                                             Text(option)
-                                                .padding(.horizontal, 16)
+                                                .padding(.horizontal, 20)
                                                 .padding(.vertical, 10)
                                                 .frame(maxWidth: .infinity)
-                                                .background(Color.white)
                                                 .foregroundColor(.blue)
-                                                .cornerRadius(18)
+                                                .background(
+                                                    Capsule()
+                                                        .fill(.greenGray2)
+                                                )
+                                                .overlay(
+                                                    Capsule()
+                                                        .stroke(.greenGray5, lineWidth: 1)
+                                                )
                                         }
                                     }
                                     .buttonStyle(PlainButtonStyle())
@@ -65,10 +88,16 @@ struct MessageRow: View {
                             }
                         }
                         .padding(.horizontal, 16)
-                        .padding(.vertical, 10)
-                        .background(Color.gray.opacity(0.2))
-                        .foregroundColor(.primary)
-                        .cornerRadius(18)
+                        .padding(.vertical, 11)
+                        .background(.gray2)
+                        .clipShape(
+                            UnevenRoundedRectangle(
+                                topLeadingRadius: 0,
+                                bottomLeadingRadius: 12,
+                                bottomTrailingRadius: 12,
+                                topTrailingRadius: 12
+                            )
+                        )
                         .frame(maxWidth: 250, alignment: .leading)
                     }
                 }
