0d9c9ae21823ca7716f0d207abe5cbcb54a9bf01
Lee Inho <inhoking01@gmail.com>
2026-01-29 01:07:57 +0900

    refactor. 리포트 화면 디자인 수정

--- OMT/Features/Report/ReportView.swift
+++ OMT/Features/Report/ReportView.swift
@@ -14,33 +14,33 @@ struct ReportView: View {
     
     var body: some View {
         ZStack {
-            VStack {
+            VStack(spacing: 12) {
                 Text("OMT")
                 
                 ScrollView {
-                    VStack {
-                        HStack {
-                            weekNavigationHeader
-                            Spacer()
-                            Button {
-                                
-                            } label: {
-                                Image(systemName: "arrow.clockwise.circle")
+                    VStack(spacing: 28) {
+                        VStack(spacing: 20) {
+                            HStack {
+                                weekNavigationHeader
+                                Spacer()
+                                Button {
+                                    
+                                } label: {
+                                    Image(systemName: "arrow.clockwise.circle")
+                                }
                             }
+                            
+                            successRateCard
+                            topDifficultiesCard
+                            recommendCard
                         }
-                        
-                        successRateCard
-                        
-                        topDifficultiesCard
-                        
-                        recommendCard
                         
                         analysisDetailButton
                     }
-                    .padding()
+                    .padding(.horizontal, 20)
                 }
             }
-            .background(.gray)
+            .background(.gray2)
             
             if store.isDatePickerPresented {
                 DatePickerModal(store: store)
@@ -51,24 +51,28 @@ struct ReportView: View {
 
 extension ReportView {
     private var weekNavigationHeader: some View {
-        VStack(alignment: .leading) {
+        VStack(alignment: .leading, spacing: 12) {
             HStack {
                 Button {
                     store.send(.previousWeekTapped)
                 } label: {
-                    Image(systemName: "chevron.left")
+                    Image("arrow_back_02")
                 }
                 
-                Button {
-                    store.send(.dateTapped)
-                } label: {
-                    Text(store.displayText)
+                HStack(spacing: 4) {
+                    Button {
+                        store.send(.dateTapped)
+                    } label: {
+                        Text(store.displayText)
+                            .typography(.sub_btn2_enabled)
+                            .foregroundStyle(.gray11)
+                    }
                 }
                 
                 Button {
                     store.send(.nextWeekTapped)
                 } label: {
-                    Image(systemName: "chevron.right")
+                    Image("arrow_forth")
                 }
             }
         }
@@ -77,24 +81,36 @@ extension ReportView {
 
 extension ReportView {
     private var successRateCard: some View {
-        VStack {
-            HStack {
-                Image("")
-                VStack(alignment: .leading) {
+        VStack(alignment: .leading, spacing: 0) {
+            HStack(spacing: 16) {
+                Image("icon_dart")
+                    .resizable()
+                    .frame(width: 56, height: 56)
+                
+                VStack(alignment: .leading, spacing: 8) {
                     Text("이번주 미션 성공률")
+                        .foregroundStyle(.gray10)
+                    
                     Text("57%")
+                        .typography(.h1)
+                        .foregroundStyle(.gray13)
                 }
             }
-            HStack {
+            .padding(.vertical, 10)
+            
+            HStack(spacing: 12) {
                 ForEach(weekdays, id: \.self) { day in
-                    VStack {
+                    VStack(spacing: 12) {
                         Text(day)
+                            .typography(.sub_b4_3)
+                            .foregroundStyle(.gray8)
                         Circle()
                     }
                 }
             }
+            .padding(.vertical, 10)
         }
-        .padding()
+        .padding(.horizontal, 12)
         .frame(maxWidth: .infinity)
         .background(.white)
         .cornerRadius(16)
@@ -103,8 +119,14 @@ extension ReportView {
 
 extension ReportView {
     private var topDifficultiesCard: some View {
-        VStack(alignment: .leading) {
-            Text("Top2")
+        VStack(alignment: .leading, spacing: 20) {
+            HStack(spacing: 8) {
+                Image("icon_good")
+                
+                Text("미션이 힘들었던 이유를 살펴봤어요!")
+                    .typography(.h4)
+                    .foregroundStyle(.gray11)
+            }
             
             if store.topDifficulties.isEmpty {
                 Text("데이터가 없음")
@@ -123,8 +145,14 @@ extension ReportView {
 
 extension ReportView {
     private var recommendCard: some View {
-        VStack(alignment: .leading) {
-            Text("제안")
+        VStack(alignment: .leading, spacing: 14) {
+            Text("OMT의 제안")
+                .typography(.h3)
+                .foregroundStyle(.gray11)
+            
+            Text("다음주에는 수요일을 쉬는 날로 두고, 목・금요일에는 간단한 미션을 선택하는 건 어때요?")
+                .typography(.sub_b2_2)
+                .foregroundStyle(.gray10)
         }
         .padding()
         .frame(maxWidth: .infinity, alignment: .leading)
@@ -141,9 +169,11 @@ extension ReportView {
             Text("더 자세한 분석 보기")
                 .padding()
                 .frame(maxWidth: .infinity)
+                .typography(.btn2_enabled)
+                .foregroundStyle(.gray12)
                 .background(
-                    RoundedRectangle(cornerRadius: 12)
-                        .fill(.primary5)
+                    RoundedRectangle(cornerRadius: 10)
+                        .fill(.primary7)
                 )
                 .contentShape(Rectangle())
         }
