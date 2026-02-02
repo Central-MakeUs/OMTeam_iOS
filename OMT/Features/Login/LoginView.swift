432e9ed1c441d7f4e86e88ed94f573e917a53912
Lee Inho <inhoking01@gmail.com>
2026-02-03 01:18:34 +0900

    chore. 로그인 화면 디자인

--- OMT/Features/Login/LoginView.swift
+++ OMT/Features/Login/LoginView.swift
@@ -12,24 +12,48 @@ struct LoginView: View {
     let store: StoreOf<LoginFeature>
     
     var body: some View {
-        VStack(spacing: 20) {
+        ZStack {
+            Color.primary2
+                .ignoresSafeArea()
+            
+            VStack {
+                Spacer()
+                VStack(spacing: 33) {
+                    Image("loginCharacter")
+                        .padding(.leading, 32)
+                    
+                    Text("OMT를 시작하려면 로그인이 필요해요 :)")
+                        .typography(.sub_b1)
+                        .foregroundStyle(.gray9)
+                }
+                Spacer()
+                loginButtons
+            }
+        }
+    }
+}
+
+extension LoginView {
+    private var loginButtons: some View {
+        VStack(spacing: 12) {
             Button {
                 store.send(.appleLoginTapped)
             } label: {
-                Text("애플 로그인")
+                Image("applelogin")
             }
             
             Button {
                 store.send(.kakaoLoginTapped)
             } label: {
-                Text("카카오 로그인")
+                Image("kakaologin")
             }
             
             Button {
                 store.send(.googleLoginTapped)
             } label: {
-                Text("구글 로그인")
+                Image("googlelogin")
             }
         }
+        .padding(.bottom, 22)
     }
 }
