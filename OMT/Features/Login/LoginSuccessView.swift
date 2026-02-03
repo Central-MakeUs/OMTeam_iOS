4d391cfb17b638b5cc058a7361cf45999c3554ab
Lee Inho <inhoking01@gmail.com>
2026-02-03 23:48:18 +0900

    feat. 계정 연동 화면 구현

---
+++ OMT/Features/Login/LoginSuccessView.swift
@@ -0,0 +1,53 @@
+//
+//  LoginSuccessView.swift
+//  OMT
+//
+//  Created by 이인호 on 2/3/26.
+//
+
+import SwiftUI
+import ComposableArchitecture
+
+struct LoginSuccessView: View {
+    let store: StoreOf<LoginFeature>
+    
+    var body: some View {
+        VStack {
+            Spacer()
+            
+            VStack(spacing: 40) {
+                Image("linkSuccess")
+                
+                VStack(spacing: 16) {
+                    Text("계정 연동이 완료되었어요!")
+                        .typography(.h1)
+                        .foregroundStyle(.gray13)
+                    
+                    HStack(spacing: 0) {
+                        Text("OMT")
+                            .typography(.sub_b2_1)
+                            .foregroundStyle(.primary8)
+                        Text("와함께 건강한 습관을 만들어봐요")
+                            .typography(.sub_b2_1)
+                            .foregroundStyle(.gray10)
+                    }
+                }
+            }
+            
+            Spacer()
+            
+            Button {
+                
+            } label: {
+                Text("OMT 시작하기")
+                    .frame(maxWidth: .infinity)
+                    .frame(height: 56)
+                    .typography(.btn2_enabled)
+                    .foregroundColor(.gray12)
+                    .background(.primary7)
+                    .clipShape(RoundedRectangle(cornerRadius: 10))
+            }
+        }
+        .padding(.horizontal, 20)
+    }
+}
