3ad535f975c2465f5778644240e4397d5c91d98b
Lee Inho <inhoking01@gmail.com>
2026-02-11 21:37:57 +0900

    fix. 애니메이션이 적용되지 않아 최상위로 변경

--- OMT/Features/Root/RootView.swift
+++ OMT/Features/Root/RootView.swift
@@ -23,35 +23,33 @@ struct RootView: View {
             }
         }
         .animation(.easeInOut(duration: 0.3), value: store.showSplash)
+        .animation(.easeInOut(duration: 0.3), value: store.currentView)
     }
 
     @ViewBuilder
     private var mainContent: some View {
-        Group {
-            switch store.currentView {
-            case .login:
-                LoginView(store: store.scope(
-                    state: \.login,
-                    action: \.login)
+        switch store.currentView {
+        case .login:
+            LoginView(store: store.scope(
+                state: \.login,
+                action: \.login)
+            )
+        case .loginSuccess:
+            LoginSuccessView(store: store.scope(
+                state: \.login,
+                action: \.login)
+            )
+        case .onboarding:
+            if let onboardingStore = store.scope(
+                state: \.onboarding,
+                action: \.onboarding
+            ) {
+                OnboardingView(
+                    store: onboardingStore
                 )
-            case .loginSuccess:
-                LoginSuccessView(store: store.scope(
-                    state: \.login,
-                    action: \.login)
-                )
-            case .onboarding:
-                if let onboardingStore = store.scope(
-                    state: \.onboarding,
-                    action: \.onboarding
-                ) {
-                    OnboardingView(
-                        store: onboardingStore
-                    )
-                }
-            case .home:
-                CustomTabView(store: store)
             }
+        case .home:
+            CustomTabView(store: store)
         }
-        .animation(.easeInOut(duration: 0.3), value: store.currentView)
     }
 }
